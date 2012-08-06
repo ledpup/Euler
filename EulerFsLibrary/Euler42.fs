9d026a7ef2264974155fbbbfe343d62d613012ee
Patrick Lea <ledpup@gmail.com>
2012-08-06 20:10:25 +1000

    Euler 42

---
+++ EulerFsLibrary/Euler42.fs
@@ -0,0 +1,29 @@
+﻿namespace EulerFsLibrary
+
+open System.IO
+
+module Euler42 = 
+
+    let charValue (c:char) = (int c) - (int 'A') + 1
+
+    let stringValue (s:string) = s.ToCharArray()
+                                        |> List.ofArray
+                                        |> List.map (fun x -> charValue (x))
+                                        |> List.sum
+
+    let triangleNumber x = x * (x + 1) / 2
+
+    let words = File.ReadAllText(@"C:\Users\ledpup\Documents\GitHub\Euler\EulerFsLibrary\words.txt").Replace("\"","").Split(',')
+                                                    |> List.ofArray
+
+    let triangleNumbers = {1..100}
+                           |> Seq.map (fun x -> triangleNumber x)
+
+    let isTriangleNumber x = triangleNumbers
+                                |> Seq.exists(fun y -> y = x)
+
+    let triangleWords = words 
+                            |> List.map (fun x -> stringValue x)
+                            |> List.filter (fun x -> (isTriangleNumber x))
+
+    let sumTriangleWords = triangleWords |> List.length
