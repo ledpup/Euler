a9e2d0e28707ff1da655672f01e4d9f53f20fea4
Patrick Lea <ledpup@gmail.com>
2012-08-06 17:01:17 +1000

    Euler 36

---
+++ EulerFsLibrary/Euler36.fs
@@ -0,0 +1,21 @@
+﻿namespace EulerFsLibrary
+
+open System
+
+module Euler36 =
+
+    let revNumberToCharArray x = x.ToString().ToCharArray()
+                                |> Array.rev
+
+    let revNumberToString x = String.Join(null, revNumberToCharArray x)
+
+    let isPalindrome x = x.ToString() = revNumberToString x
+
+    let intAsBinary (x:int) = Convert.ToString(x, 2)
+
+    let five = "101" = intAsBinary 5
+
+    let numbersThatArePalindromeInDecimalAndBinary = [1..1000000]
+                                                        |> List.filter (fun x -> isPalindrome x && isPalindrome (intAsBinary x))
+    
+    let sumPalindromes = numbersThatArePalindromeInDecimalAndBinary |> List.sum
