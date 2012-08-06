bf161b67a6423359aff9f874f611a14b36a28336
Patrick Lea <ledpup@gmail.com>
2012-08-06 15:16:38 +1000

    Euler 22

--- EulerFsLibrary/Euler12.fs
+++ EulerFsLibrary/Euler12.fs
@@ -2,5 +2,22 @@
 
 module Euler12 = 
     let triangle x = x * (x + 1) / 2
+                       
+    let factors number = seq {
+        for divisor in 1 .. (float >> sqrt >> int) number do
+        if number % divisor = 0 then
+            yield divisor
+            yield number / divisor
+    }
 
-    let t1 = triangle 7
+    let countDivisors x = factors x
+                                |> Seq.length
+
+    factors (triangle 12375) |> Seq.iter (printf "%d ")
+    printfn "%A" (countDivisors (triangle 12375))
+    
+    let triangleWithXDivisors factors = seq { 1..1..1000000000 }
+                                                |> Seq.find (fun a -> (countDivisors (triangle a)) > factors)
+
+    printfn "%A" (triangleWithXDivisors 500 |> triangle) 
+
---
+++ EulerFsLibrary/Euler22.fs
@@ -0,0 +1,25 @@
+﻿namespace EulerFsLibrary
+
+open System.IO
+
+module Euler22 = 
+    let charValue (c:char) = (int c) - (int 'A') + 1
+
+    let stringValue (s:string) = s.ToCharArray()
+                                            |> List.ofArray
+                                            |> List.map (fun x -> charValue (x))
+                                            |> List.sum
+
+    let Colin = stringValue "COLIN" = 53
+
+    let sortedNames = File.ReadAllText(@"C:\Code\GitHub\Euler\EulerFsLibrary\names.txt").Replace("\"","").Split(',')
+                                                    |> List.ofArray
+                                                    |> List.sort
+
+    let namesScore x = x 
+                        |> List.mapi (fun index element -> index, element)
+                        |> List.map (fun (index, element) -> (index + 1) * (stringValue element))
+
+    let colinScore = (namesScore sortedNames).Item(937) = 49714
+
+    printfn "%A" ((namesScore sortedNames) |> List.sum)
