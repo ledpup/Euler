9ba3aabf9cd4c11c0a97d47ddfba886717cd20d8
Patrick Lea <ledpup@gmail.com>
2012-08-13 15:49:25 +1000

    Refactor Euler 25 out into it's own file.

--- EulerFsLibrary/Euler.fs
+++ EulerFsLibrary/Euler.fs
@@ -178,28 +178,4 @@ module Euler20 =
             |> Seq.map (string >> int)
             |> Seq.sum
 
-module Euler25 =
-    let rec fibonaci x =
-            match x with
-            | x when x < 0 -> failwith "Value cannot be less than 0."
-            | 1 | 2 -> 1
-            | x -> fibonaci(x - 1) + fibonaci(x - 2)
-
-    let rec forwardFib (current:bigint) (previous:bigint) count = match current with
-                                                                    | current when current.ToString().Length = 1000 -> count
-                                                                    | _ -> forwardFib (current + previous) current (count + 1)
-
-    let result = forwardFib 1I 1I 2
-
-    let fib = let mutable previous:bigint = 1I
-              let mutable current:bigint = 1I
-              let mutable temp:bigint = 0I
-              let mutable count = 2
-              while current.ToString().Length <> 1000 do
-                    temp <- current
-                    current <- previous + current
-                    previous <- temp
-                    count <- count + 1
-              count
-
     
