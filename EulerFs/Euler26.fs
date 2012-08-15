3d4626165c52b76e549c1fe45672f4ed4c7d3088
Patrick Lea <ledpup@gmail.com>
2012-08-15 17:04:09 +1000

    Euler 40 and starting 26

---
+++ EulerFs/Euler26.fs
@@ -0,0 +1,11 @@
+﻿namespace EulerFs
+
+module Euler26 = 
+    let repetitionLength (number:int64) = let numberList = number.ToString().ToCharArray()
+                                                                        |> List.ofArray
+                                          numberList
+
+    repetitionLength 123456123456L
+
+
+    [1..1000] |> List.map (fun x -> 1.0 / (float x)) |> List.iter (fun x -> printfn "%A" x)
--- EulerFs/Euler40.fs
+++ EulerFs/Euler40.fs
@@ -1,7 +1,21 @@
 ﻿namespace EulerFs
 
+open System.Collections.Generic
+
 module Euler40 =
-    let rec numberBuilder (number:string) (count:bigint) = if number.Length = 1000000 then number else numberBuilder (number + count.ToString()) (count + 1I)
+    let processNumber number baseIndex exp = let nthExponent = (pown 10 exp)
+                                             number.ToString().ToCharArray()
+                                                |> List.ofArray
+                                                |> List.mapi (fun index el -> if (baseIndex + index) % nthExponent = 0 then (int el - int '0') else -1)
+                                                |> List.filter (fun x -> x >= 0)
 
-    let stupidNumber = numberBuilder "" 0I
+    let numbers = new List<int>()
 
+    let rec numberBuilder number index exp = let result = processNumber number index exp
+                                             let newExp = if result = [] then exp else exp + 1
+                                             if result <> [] then numbers.Add(result |> List.head)
+                                             if number < 1000000 then numberBuilder (number + 1) (index + number.ToString().Length) newExp 
+    
+    numberBuilder 1 1 1
+
+    let finalResult = Seq.fold (fun acc el -> el * acc) 1 numbers
