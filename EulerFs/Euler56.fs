00458e7333a4ff5e632ec12258053d6d813605ed
Patrick Lea <ledpup@gmail.com>
2012-08-17 15:09:52 +1000

    Euler 56

---
+++ EulerFs/Euler56.fs
@@ -0,0 +1,15 @@
+﻿namespace EulerFs
+
+module Euler56 =
+    let digitSum number = number.ToString().ToCharArray()
+                                    |> Array.map (fun x -> int x - int '0')
+                                    |> Array.sum
+
+    let _55 = digitSum 55 = 10
+
+    let cartesian xs ys = xs |> List.collect (fun x -> ys |> List.map (fun y -> x, y))
+
+    let dsadjkh = cartesian [1I..99I] [1..99]
+                        |> List.map (fun (x, y) -> x ** y)
+                        |> List.map (fun x -> digitSum x)
+                        |> List.max
