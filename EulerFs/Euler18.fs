f8be6e9015acab630c9b76a57bd527cf3154136a
Patrick Lea <ledpup@gmail.com>
2012-08-24 17:58:51 +1000

    Starting Euler 18

---
+++ EulerFs/Euler18.fs
@@ -0,0 +1,11 @@
+﻿namespace EulerFs
+
+module Euler18 =
+
+    let sampleData = [| [3;]; [7; 4;]; [2; 4; 6;]; [8; 5; 9; 3;]; |]
+
+    let findHighest numberList = numberList |> Seq.windowed 2 |> Seq.mapi (fun i el -> i, el |> Seq.max)
+
+    let highest = findHighest [8; 5; 9; 3;]
+
+    printfn "%A" highest
